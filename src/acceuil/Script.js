c02f520c66322353379ae0de79892c8873016305
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2024-10-10 11:13:55 +0200

    ajout de la fonction cacher contactUs

--- src/acceuil/Script.js
+++ src/acceuil/Script.js
@@ -1,32 +1,14 @@
-const buttonTask = document.querySelector(`#addTask`);
-const ulTask = document.querySelector(`#taskArea`);
+const taskData = [];
+const listLibrary = [];
 
-buttonTask.addEventListener('click', () => {
-    const text = document.querySelector('#textTask').value;
-    const li = document.createElement('li');
-    li.innerHTML = text;
-    ulTask.append(li);
-})
-
-
-const buttonList = document.querySelector(`#addList`);
-const ulList = document.querySelector(`#listArea`);
-
-buttonList.addEventListener('click', () => {
-    const text = document.querySelector('#textList').value;
-    const li = document.createElement('li');
-    li.innerHTML = text;
-    ulList.append(li);
-})
-
-const taskTemplate = {
+const task = {
   title :null,
   statut: null,
   categorie: null,
   deadline: null,
 }
 
-const listTemplate = {
+const list = {
   title :null,
   statut: null,
   categorie: null,
@@ -38,3 +20,53 @@ const qualifications = {
   categorie: ['priority','low','high'],
   deadline: null,
 }
+
+const buttonTask = document.querySelector(`#addTask`);
+const ulTask = document.querySelector(`#taskArea`);
+
+buttonTask.addEventListener('click', () => {
+    const text = document.querySelector('#textTask').value;
+    const li = document.createElement('li');
+    task
+    li.innerHTML = text;
+    ulTask.append(li);
+})
+
+
+// J'écoute le buttonTask pour créer une tache dans ma taskArea//
+// const buttonTask = document.querySelector(`#addTask`);
+// const ulTask = document.querySelector(`#taskArea`);
+
+// buttonTask.addEventListener('click', () => {
+//     const text = document.querySelector('#textTask').value;
+//     const li = document.createElement('li');
+//     li.innerHTML = text;
+//     ulTask.append(li);
+// })
+
+// J'écoute le buttonList pour créer une tache dans ma listArea//
+const buttonList = document.querySelector(`#addList`);
+const ulList = document.querySelector(`#listArea`);
+
+buttonList.addEventListener('click', () => {
+    const text = document.querySelector('#textList').value;
+    const li = document.createElement('li');
+    li.innerHTML = text;
+    ulList.append(li);
+})
+
+
+// J'affiche ma section Contact Us en cliquant sur le bouton Contact Us//
+ const contactUsButton = document.querySelector(`#contactUsButton`);
+ const contactUsArea = document.querySelector(`.contactUsArea`);
+ 
+ contactUsButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  if (contactUsArea.style.display === "block") {
+    contactUsArea.style.display = "none";
+  } else  {
+    contactUsArea.style.display = "block";
+  }
+});
