920439f098c7a11b7c42b504305da54eff539229
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2024-10-11 17:43:35 +0200

    ajout fonction carte de tache

--- src/acceuil/Script.js
+++ src/acceuil/Script.js
@@ -10,6 +10,7 @@ let priorityInput = document.getElementById(`priorityInput`);
 let statutInput = document.getElementById(`statutInput`);
 let taskListInput = document.getElementById(`taskOrListInput`);
 let taskContainer = document.getElementById(`cardContainer`);
+let taskData = [];
 
 // Etape 2: J'écoute les élements:
 
@@ -25,20 +26,20 @@ form.addEventListener('submit', function(event) {
 
   // je crée mon element à insérer lorsque je crée une tâche:
     const taskElement = document.createElement('div');
+    taskData.push(text);
     taskElement.classList.add('task');
-
     taskElement.innerHTML = `
     <div class="cardContainer">
       <h3>${name}</h3>
-      <div>
+    <div>
       <p>Catégorie: ${priority}</p>
       <p>Deadline: ${deadline}</p>
-      </div>
+    </div>
       <p>Statut: ${statut}</p>
       <p>Catégorie: ${taskOrList}</p>
     </div>
     `;
-  
+
     // j'insère mon élément dans le conteneur:
   taskContainer.appendChild(taskElement);
 
@@ -50,14 +51,7 @@ form.addEventListener('submit', function(event) {
 // // =======================> CREATION DE LA FUNCTION : CREATION D'OBJETS AVEC PLUSIEURS VARIABLES
 
 // // Etape 1: Définir son besoin: je veux que pour chaque nouvelle entrée de tache, un object se crée:
-// buttonTask.addEventListener('click', () => {
-//   const text = document.querySelector('#textInput').value;
-//   const li = document.createElement('li');
-//   taskData.push(text);
-//   console.log(taskData);
-//   li.innerHTML = text;
-//   ulTask.append(li);
-// })
+
 
 
 
